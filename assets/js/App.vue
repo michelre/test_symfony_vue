46c0e58abccc05192d5ee46137f826a2996b1e83
Remi Michel <remi.michel-externe@bio-logic.net>
2018-05-15 10:54:20 +0200

    start the project with Symfony and Vue.js

---
+++ assets/js/App.vue
@@ -0,0 +1,15 @@
+<style>
+  @import '../scss/app.scss';
+</style>
+
+<template>
+  <h1>Hello {{name}}!</h1>
+</template>
+
+<script>
+  export default {
+    data(){
+      return { name: 'Rémi' }
+    }
+  }
+</script>
